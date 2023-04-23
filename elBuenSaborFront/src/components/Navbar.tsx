737937f2afe5a00d1e73b950e7b9b1c0ca6a2f8f
pazfernandez <mariapazfernandezravaglia@gmail.com>
2023-04-22 21:07:26 -0300

    clases de bootstrap añadidas al navbar

--- elBuenSaborFront/src/components/Navbar.tsx
+++ elBuenSaborFront/src/components/Navbar.tsx
@@ -21,24 +21,25 @@ console.log(isAuthenticated);
 
   if(isAuthenticated){
     return(
-      <header className="headerLogo" style={{justifyContent: "space-around", flexDirection: "row"}}>
+      <header className="headerLogo" style={{justifyContent: "space-around", flexDirection: "row", color:"white"}}>
         <div className="logo" style={{display: "inline-flex", alignItems: "center"}}>
-        <i className="material-icons" style={{fontSize: "30px", marginTop: "5%", color:"white", marginLeft: "3%"}}> search</i>
+          <i className="ml-3 mt-3 material-icons" style={{fontSize: "30px", color:"white", cursor:"pointer"}}> search</i>
           <input className="btnIngresoNav inputs" type="text" placeholder="Buscar producto" >
           </input>
         </div>
         {/*<div style={{flexDirection: "row", justifyContent: "flex-end"}}>*/}
          
-            <div >
-              <i className="material-icons" style={{fontSize: "30px", marginTop: "5%", color:"white"}}> shopping_cart</i>
-              <i className="material-icons" style={{fontSize: "30px", marginTop: "5%", color:"white"}}> favorite</i>
+            <div className='mt-3'>
+              <i className="material-icons" style={{fontSize: "30px", cursor:"pointer"}}> shopping_cart</i>
+              <i className="material-icons" style={{fontSize: "30px", cursor:"pointer"}}> favorite</i>
+            </div>
+            
+            <div>
+              <h5 className="mt-3" >{user!.name}</h5>
+
             </div>
             <div>
-              <><img style={{borderRadius: "50%", maxWidth: "50%", maxHeight:"50%"}} src={user!.picture} alt="imagen de perfil" /></>
-            </div>
-            <div>
-              <h3>{user!.name}</h3>
-              <h3>{user!.id}</h3>
+              <><img style={{borderRadius: "50%", maxWidth: "60%", maxHeight:"60%", cursor:"pointer"}} src={user!.picture} alt="imagen de perfil" /></>
             </div>
           <div><LogoutBtn/></div>
         {/*</div>*/}
@@ -52,17 +53,19 @@ console.log(isAuthenticated);
 
     return(
       <header className="headerLogo">
-        <div className="logo" style={{display: "inline-flex", alignItems: "center"}}>
-        <i className="material-icons" style={{fontSize: "30px", marginTop: "5%", color:"white", marginLeft: "3%"}}> search</i>
+        <div className="logo mx-3 mt-2" style={{display: "inline-flex", alignItems: "center"}}>
+          <i className="material-icons" style={{fontSize: "30px", color:"white"}}> search</i>
           
-        <input className="btnIngresoNav inputs" type="text" placeholder="Buscar producto" >
+          <input className="btnIngresoNav inputs" type="text" placeholder="Buscar producto" >
           </input>
         </div>
-        <nav>
-          <i className="material-icons" style={{fontSize: "30px", marginTop: "5%", color:"white"}}> shopping_cart</i>
+        <nav className="row mt-2">
+          <i className="col-sm mt-3 material-icons" style={{fontSize: "30px", color:"white"}}> shopping_cart</i>
           
+          <div className="col-sm mx-1">
+            <LoginBtn/>
+          </div>
           
-          <LoginBtn/>
           
           
         </nav>
--- elBuenSaborFront/src/main.tsx
+++ elBuenSaborFront/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './css/index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 

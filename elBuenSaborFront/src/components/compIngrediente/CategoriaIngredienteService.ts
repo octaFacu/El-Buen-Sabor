c8bd651621a2d50a479a6746d1d9b58137daeb04
pazfernandez <mariapazfernandezravaglia@gmail.com>
2023-04-26 19:52:13 -0300

    Update CategoriaIngredienteService.ts

--- elBuenSaborFront/src/components/compIngrediente/CategoriaIngredienteService.ts
+++ elBuenSaborFront/src/components/compIngrediente/CategoriaIngredienteService.ts
@@ -1,3 +1,5 @@
+import { Rubro } from "./Rubro"
+
 export  class CategoriaIngredienteService{
     
     baseUrl = "http://localhost:8080/categoria"
@@ -27,6 +29,31 @@ export  class CategoriaIngredienteService{
         try {
             
             let res = await fetch(this.baseUrl+"/padres")
+
+            if(!res.ok){
+                throw {status: res.status, statusText: res.statusText}
+            }
+    
+            let jsonRes = await res.json()
+            return jsonRes
+
+        } catch (err: any) {
+            console.log(`Error ${err.status}: ${err.statusText}`);
+        }
+    }
+
+    async updateCategoriaIngrediente(updatedCat: Rubro){
+
+        
+        try {
+            
+            let res = await fetch(this.baseUrl, {
+                method: "PUT",
+                headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(updatedCat),
+              });
 
             if(!res.ok){
                 throw {status: res.status, statusText: res.statusText}

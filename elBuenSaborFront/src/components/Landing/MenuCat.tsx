6e0ba5fc1e92ef80c236d2c172d4e3ff437fdf63
pazfernandez <mariapazfernandezravaglia@gmail.com>
2023-05-26 23:01:00 -0300

    sass comentado temporal-

--- elBuenSaborFront/src/components/Landing/MenuCat.tsx
+++ elBuenSaborFront/src/components/Landing/MenuCat.tsx
@@ -19,14 +19,6 @@ const MenuCat: React.FC = () => {
 
     return(
         <div>
-        {/* <div className="menuCat">
-            <div><button className="btnPag"><i className="material-icons" style={{fontSize: "30px"}}> keyboard_arrow_left</i></button></div>
-            <div style={{fontSize: "24px"}}><a><i className="material-icons" > local_dining</i> Hamburguesas</a></div>
-            <div style={{fontSize: "24px"}}><a><i className="material-icons"> local_dining</i> Pizza </a></div>
-            <div style={{fontSize: "24px"}}><a><i className="material-icons"> local_dining</i> Lomos</a></div>
-            <div style={{fontSize: "24px"}}><a><i className="material-icons"> local_dining</i> Bebidas</a></div>
-            <div style={{fontSize: "24px"}}><button className="btnPag"><i className="material-icons" style={{fontSize: "30px"}}> keyboard_arrow_right</i></button></div>
-        </div> */}
 
 
         <div className="menuCat">

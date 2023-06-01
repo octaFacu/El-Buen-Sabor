cdbe1bee48013f1a1f829910999257145aa85a92
Octavio <octavio.curadelli@alumnos.frm.utn.edu.ar>
2023-06-01 00:21:25 -0300

    Rutas actualizadas

--- elBuenSaborFront/src/routes/AppRoutes.tsx
+++ elBuenSaborFront/src/routes/AppRoutes.tsx
@@ -3,15 +3,22 @@ import { LandingRoutes } from "./LandingRoutes"
 import { NotFound } from "../components/NotFound"
 import { AbmEjemploRoutes } from "../routes/AbmEjemploRoutes"
 import UserRouters from '../routes/UserRouters'
+import { routesConfig } from "./routesConfig"
 
 export const AppRoutes = () => {
     return (
 
+        // <Routes>
+        //     <Route path="/" element={<LandingRoutes />} />
+        //     <Route path="/abm/*" element={<AbmEjemploRoutes />} />
+        //     <Route path="/usuarios" element={<UserRouters />}/>
+        //     <Route path="*" element={<NotFound />} />
+        // </Routes>
+
         <Routes>
-            <Route path="/" element={<LandingRoutes />} />
-            <Route path="/abm/*" element={<AbmEjemploRoutes />} />
-            <Route path="/usuarios" element={<UserRouters />}/>
-            <Route path="*" element={<NotFound />} />
+            {routesConfig.map((route, i) => (
+                <Route key={i} path={route.path} element={<route.component />} />
+            ))}
         </Routes>
 
     )

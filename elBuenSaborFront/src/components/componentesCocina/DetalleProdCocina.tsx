db035959165a0d786a421cb098bc7cc72f5c02e9
pazfernandez <mariapazfernandezravaglia@gmail.com>
2024-01-08 16:58:26 -0300

    formateo necesario

--- elBuenSaborFront/src/components/componentesCocina/DetalleProdCocina.tsx
+++ elBuenSaborFront/src/components/componentesCocina/DetalleProdCocina.tsx
@@ -22,87 +22,88 @@ const DetalleProdCocina: React.FC<ProdFormProps> = ({ producto, estado, changeEs
     const { rol } = useUnidadContext();
 
     const prodService = new ProductoService();
-    const [ingredientes, setIngredientes] = useState<IngredienteDeProdCompleto[]>([]); 
+    const [ingredientes, setIngredientes] = useState<IngredienteDeProdCompleto[]>([]);
     const [loading, setLoading] = useState(true);
-    
-    const getIngredientes = async() => {
+
+    const getIngredientes = async () => {
         try {
             await prodService.getIngredientes(producto.id!, rol).then((data) => {
-                console.log("Id del producto: "+producto.id)
+                console.log("Id del producto: " + producto.id)
                 setIngredientes(data);
-                ingredientes.map((ing, index) => ("ing: "+console.log(ing.ingrediente.id)));
+                ingredientes.map((ing, index) => ("ing: " + console.log(ing.ingrediente.id)));
             })
         } catch (error) {
             console.log("Ingredientes no pudieron ser traidos");
         }
     }
 
-   
+
 
 
     useEffect(() => {
 
-        if(!(producto.id === null || producto.id === 0)){
+        if (!(producto.id === null || producto.id === 0)) {
             getIngredientes();
             console.log("Han cambiado los ingredientes...");
         }
-        
+
     }, [producto, estado]);
 
     useEffect(() => {
-        if(ingredientes.length > 0 || producto.esManufacturado == false){
+        if (ingredientes.length > 0 || producto.esManufacturado == false) {
             setLoading(false);
         }
     }, [ingredientes]);
 
-    if(loading && estado) {
+    if (loading && estado) {
         return <PageLoader></PageLoader>
     }
 
-    if(!loading){
-        console.log("ENTRANDO"+ ingredientes.length)
+    if (!loading) {
+        console.log("ENTRANDO" + ingredientes.length)
 
 
-    return (
+        return (
 
-        <>
-            {estado &&
-                <div className="overlay" onClick={() => changeEstado(!estado)}>
-                    <div className="container productoContainer my-5 mt-4 mb-4 contenedorModal modaloverflow" onClick={e => e.stopPropagation()} style={{ borderRadius: "25px", backgroundColor: "#f99132", color: "white", maxWidth: "50%" }}>
-                        <h2 className="mt-3 productoTitle pt-2 pb-2">{producto.denominacion}</h2>
-                        <p className="mt-3 productoTitle pt-2 pb-2"></p>
-                        
-                        <h3 className="text-decoration-underline">Receta</h3>
-                        <p className="ms-2 me-2">{producto.receta}</p>
-
-                        <h3 className="text-decoration-underline">Ingredientes</h3>
-                        <ul>
-                        {ingredientes.map((ing, index) => (
-                            <>
-                            {/* <h1>{ing.idIngrediente}</h1> */}
-                            <IngredientesDetComponent ingredienteProd={ing} cantidad={ing.cantidad} productoId={producto.id!} estado={estado}></IngredientesDetComponent>
-                        </>
-                        ))}
-                        </ul>
-                        <div className="productoContainer tiempoProducto">
-                            <h4>{producto.tiempoCocina}</h4>
-                        </div>
-                        <div>
-                            <img className="imagen-pedido border border-white rounded border-4 me-2 ms-4 mt-2 mb-3" src={producto.imagen}></img>
-                        </div>
-                    </div>
-                </div>
-            }
-        </>
-    )}else{
-        return(
             <>
-            {estado &&
-                <div className="overlay" onClick={() => changeEstado(!estado)}>
-                    <div className="container productoContainer my-5 mt-4 mb-4 contenedorModal modaloverflow" onClick={e => e.stopPropagation()} style={{ borderRadius: "25px", backgroundColor: "#f99132", color: "white", maxWidth: "50%" }}>
-                        <PageLoader></PageLoader>
+                {estado &&
+                    <div className="overlay" onClick={() => changeEstado(!estado)}>
+                        <div className="container productoContainer my-5 mt-4 mb-4 contenedorModal modaloverflow" onClick={e => e.stopPropagation()} style={{ borderRadius: "25px", backgroundColor: "#f99132", color: "white", maxWidth: "50%" }}>
+                            <h2 className="mt-3 productoTitle pt-2 pb-2">{producto.denominacion}</h2>
+                            <p className="mt-3 productoTitle pt-2 pb-2"></p>
+
+                            <h3 className="text-decoration-underline">Receta</h3>
+                            <p className="ms-2 me-2">{producto.receta}</p>
+
+                            <h3 className="text-decoration-underline">Ingredientes</h3>
+                            <ul>
+                                {ingredientes.map((ing, index) => (
+                                    <>
+                                        {/* <h1>{ing.idIngrediente}</h1> */}
+                                        <IngredientesDetComponent ingredienteProd={ing} cantidad={ing.cantidad} productoId={producto.id!} estado={estado}></IngredientesDetComponent>
+                                    </>
+                                ))}
+                            </ul>
+                            <div className="productoContainer tiempoProducto">
+                                <h4>{producto.tiempoCocina}</h4>
+                            </div>
+                            <div>
+                                <img className="imagen-pedido border border-white rounded border-4 me-2 ms-4 mt-2 mb-3" src={producto.imagen}></img>
+                            </div>
+                        </div>
                     </div>
-                </div>}
+                }
+            </>
+        )
+    } else {
+        return (
+            <>
+                {estado &&
+                    <div className="overlay" onClick={() => changeEstado(!estado)}>
+                        <div className="container productoContainer my-5 mt-4 mb-4 contenedorModal modaloverflow" onClick={e => e.stopPropagation()} style={{ borderRadius: "25px", backgroundColor: "#f99132", color: "white", maxWidth: "50%" }}>
+                            <PageLoader></PageLoader>
+                        </div>
+                    </div>}
             </>
         )
     }
--- elBuenSaborFront/src/components/componentesCocina/IngredientesDetComponent.tsx
+++ elBuenSaborFront/src/components/componentesCocina/IngredientesDetComponent.tsx
@@ -10,58 +10,58 @@ import IngredienteDeProdCompleto from "../../context/interfaces/IngredienteDePro
 
 
 interface TablaIngredientesProdProps {
-    ingredienteProd: IngredienteDeProdCompleto;
-    cantidad: number;
-    productoId: number;
-    estado: boolean;
-  
-  }
-  
-  const IngredientesDetComponent: React.FC<TablaIngredientesProdProps> = ({ ingredienteProd, cantidad, productoId, estado }) => {
-  
+  ingredienteProd: IngredienteDeProdCompleto;
+  cantidad: number;
+  productoId: number;
+  estado: boolean;
+
+}
+
+const IngredientesDetComponent: React.FC<TablaIngredientesProdProps> = ({ ingredienteProd, cantidad, productoId, estado }) => {
 
 
-      const [loading, setLoading] = useState(true);
 
-      const { rol } = useUnidadContext();
+  const [loading, setLoading] = useState(true);
 
-      /*const getIngrediente = async() => {
-        console.log("ingrediente id: " + ingredienteProd.idIngrediente);
-        await ingredienteService.getOne(ingredienteProd.idIngrediente, rol).then((data) => {
-          setIngrediente(data)
-          console.log("Id de la medida: "+ingredienteProd.idMedida) //esto cuando lo llamo desde
-          medidaService.getOne(ingredienteProd.idMedida, rol).then((data) => {
-            console.log("DATA MEDIDA "+data)
-            setMedidaIng(data)
-            setLoading(false);
-          });
-        }); 
+  const { rol } = useUnidadContext();
 
-    }*/
-
-    useEffect(() => {
-      if(ingredienteProd != null){
+  /*const getIngrediente = async() => {
+    console.log("ingrediente id: " + ingredienteProd.idIngrediente);
+    await ingredienteService.getOne(ingredienteProd.idIngrediente, rol).then((data) => {
+      setIngrediente(data)
+      console.log("Id de la medida: "+ingredienteProd.idMedida) //esto cuando lo llamo desde
+      medidaService.getOne(ingredienteProd.idMedida, rol).then((data) => {
+        console.log("DATA MEDIDA "+data)
+        setMedidaIng(data)
         setLoading(false);
-      }
-    }, [ingredienteProd]);
+      });
+    }); 
 
-      useEffect(() => {
-        //console.log("se van a cargar los ingredientes");
-        //getIngrediente();
-        //console.log("se cargaron los ingredientes");
-      }, []);
+}*/
 
-      if(!loading && estado){
-        return(
-        <p className="fs-5">
-          • {ingredienteProd?.ingrediente.nombre} {cantidad} {ingredienteProd?.unidadmedida.denominacion}
-        </p>
-        )
-      }else{
-        return(
-        <PageLoader></PageLoader>)
-      }
-    
+  useEffect(() => {
+    if (ingredienteProd != null) {
+      setLoading(false);
+    }
+  }, [ingredienteProd]);
 
+  useEffect(() => {
+    //console.log("se van a cargar los ingredientes");
+    //getIngrediente();
+    //console.log("se cargaron los ingredientes");
+  }, []);
+
+  if (!loading && estado) {
+    return (
+      <p className="fs-5">
+        • {ingredienteProd?.ingrediente.nombre} {cantidad} {ingredienteProd?.unidadmedida.denominacion}
+      </p>
+    )
+  } else {
+    return (
+      <PageLoader></PageLoader>)
   }
-  export default IngredientesDetComponent 
+
+
+}
+export default IngredientesDetComponent 
